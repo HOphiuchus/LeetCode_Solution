60b73900ba2e39bff611d6a90428aedc4734c63a
Ophiuchus Hahn <hophiuchus@gmail.com>
2017-04-19 10:23:45 +0200

    LC007

---
+++ Java/main/LC007ReverseInteger.java
@@ -0,0 +1,14 @@
+package main;
+
+public class LC007ReverseInteger {
+    public int reverse(int x) {
+        long target = 0;
+        for (int i = x; i != 0; i /= 10) {
+            target = target*10+i % 10;
+            if(target>Integer.MAX_VALUE|| target<Integer.MIN_VALUE){
+                return 0;
+            }
+        }
+        return (int)target;
+    }
+}
